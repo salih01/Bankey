40b403e80d544585c98a5b8ac636abd622492f13
Salih Cakmak <slhckmk0@gmail.com>
2023-11-29 20:37:32 +0300

    Update UI

--- Bankey/LoginViewController.swift
+++ Bankey/LoginViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class LoginViewController: UIViewController {
 
     let loginView = LoginView()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
